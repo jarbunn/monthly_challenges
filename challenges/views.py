8141a749ebe77c57156ff61ade24bfdc4ea12627
Jacob Bunn <jacob.bunn@appliedsystems.com>
2025-12-12 15:28:11 -0700

    Added new routes

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def jan(request):
+    return HttpResponse("eat no meat for the month")
+
+def feb(request):
+    return HttpResponse("walk for 20min every day")
